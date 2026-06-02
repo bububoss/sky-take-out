90455274bc8ac2ee5d9b6f48f5e945148be7a650
yierbubu <2945347125@qq.com>
2026-06-02 21:36:17 +0800

    编辑员工业务代码开发

--- sky-server/src/main/java/com/sky/service/EmployeeService.java
+++ sky-server/src/main/java/com/sky/service/EmployeeService.java
@@ -39,4 +39,14 @@ public interface EmployeeService {
      * @return
      */
     void startOrStop(Integer status, Long id);
+
+
+    /**
+     * 根据id查询员工
+     * @param id
+     * @return
+     */
+    Employee getById(Long id);
+
+    void update(EmployeeDTO employeeDTO);
 }
